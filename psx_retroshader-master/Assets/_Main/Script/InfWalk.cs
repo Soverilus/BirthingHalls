904136d897fcb9e5c64bbec9a6fe795d52d870dc
Soverilus <30487703+Soverilus@users.noreply.github.com>
2020-02-24 13:50:06 +0800

    Infinite Hall P1
    Infinite Hall Generation done. Need to add start/end state

---
+++ psx_retroshader-master/Assets/_Main/Script/InfWalk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfWalk : MonoBehaviour {
+    [HideInInspector]
+    public GameObject oldHall;
+
+    public GameObject end;
+    GameObject myPlayer;
+    Vector3 bounds;
+
+    GameObject myClone;
+    void Start() {
+        bounds = GetComponent<Collider>().bounds.extents;
+    }
+    void OnCollisionEnter(Collision collision) {
+        if (!myPlayer) {
+            myClone = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z + bounds.z * 2), transform.rotation);
+            myPlayer = collision.gameObject;
+            myClone.GetComponent<InfWalk>().oldHall = gameObject;
+            if (oldHall) {
+                Destroy(oldHall);
+                //replace oldHall with ending backwards hall.
+            }
+        }
+    }
+}
