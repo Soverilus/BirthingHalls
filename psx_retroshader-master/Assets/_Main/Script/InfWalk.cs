76c0d380cec9d73f4a1e9e8993cc92582a99729f
Soverilus <30487703+Soverilus@users.noreply.github.com>
2020-02-26 12:59:26 +0800

    InfhallUpdated
    -added end state (sort of)

--- psx_retroshader-master/Assets/_Main/Script/InfWalk.cs
+++ psx_retroshader-master/Assets/_Main/Script/InfWalk.cs
@@ -10,19 +10,35 @@ public class InfWalk : MonoBehaviour {
     GameObject myPlayer;
     Vector3 bounds;
 
-    GameObject myClone;
+    InfWalk cloneIW;
+    bool oldIsVisible;
+
+    [HideInInspector]
+    public GameObject myClone;
+
     void Start() {
         bounds = GetComponent<Collider>().bounds.extents;
     }
     void OnCollisionEnter(Collision collision) {
         if (!myPlayer) {
-            myClone = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z + bounds.z * 2), transform.rotation);
+            Destroy(myClone);
+            myClone = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y, transform.position.z + bounds.z * 1.75f), transform.rotation);
             myPlayer = collision.gameObject;
-            myClone.GetComponent<InfWalk>().oldHall = gameObject;
+            cloneIW = myClone.GetComponent<InfWalk>();
+           cloneIW.oldHall = gameObject;
             if (oldHall) {
-                Destroy(oldHall);
                 //replace oldHall with ending backwards hall.
+                StartCoroutine(ReplaceOldHall());
+                
+                //myClone.GetComponent<InfWalk>().myClone = Instantiate(end, oldHall.transform.position, transform.rotation);
+                //Destroy(oldHall);
             }
         }
+    }
+
+    IEnumerator ReplaceOldHall() {
+        yield return new WaitUntil(() => !oldHall.GetComponent<Renderer>().isVisible);
+        cloneIW.myClone = Instantiate(end, oldHall.transform.position, transform.rotation);
+        Destroy(oldHall);
     }
 }
