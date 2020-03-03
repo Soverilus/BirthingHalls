8626be50b3e15f55b568e1471d6b0cfe92cfab1b
Soverilus <30487703+Soverilus@users.noreply.github.com>
2020-03-03 11:57:18 +0800

    MORE LIGHTER
    - Remind Asif to Not Hyperfocus on the Lighter
    -HYperfocus more on the lighter

---
+++ psx_retroshader-master/Assets/_Main/Script/SetMyPositionToGameObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMyPositionToGameObject : MonoBehaviour {
+    public GameObject myTarget;
+    void Update() {
+        transform.position = myTarget.transform.position;
+    }
+}
