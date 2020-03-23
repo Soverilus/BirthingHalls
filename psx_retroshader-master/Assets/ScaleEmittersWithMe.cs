5acda2df8543317f7432a4050b3fdacd6bdf7930
Soverilus <30487703+Soverilus@users.noreply.github.com>
2020-03-23 18:14:37 +0800

    ENDHALL IS INFINITE
    PRAISE BE CAKE

---
+++ psx_retroshader-master/Assets/ScaleEmittersWithMe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleEmittersWithMe : MonoBehaviour {
+    public GameObject[] myEmitters;
+
+    private void Update() {
+        for (int i = 0; i < myEmitters.Length; i++) {
+            myEmitters[i].transform.localScale = transform.localScale;
+        }
+    }
+}
